5861d3f6a524400126e4b08f3007b6bdbeeac6ec
hetao <hetao@hetaodeMacBook-Pro.local>
2018-04-09 18:20:57 +0800

    list

--- components/Desks.js
+++ components/Desks.js
@@ -1,7 +1,27 @@
 import React, { Component } from 'react'
-import { View, Text, Button } from 'react-native'
+import { View, Text, Button, FlatList, StyleSheet, TouchableOpacity } from 'react-native'
+import { white, purple } from '../utils/colors'
 
 class Desks extends Component {
+
+  state = {
+    desks: [
+      {name: 'Devin', num: 0},
+      {name: 'Jackson', num: 34},
+      {name: 'James', num: 3},
+      {name: 'Joel', num: 3},
+      {name: 'John', num: 45},
+      {name: 'Jillian', num: 0},
+      {name: 'Jimmy', num: 0},
+      {name: 'Julie', num: 25},
+    ]
+  }
+
+  clickItem = (item) => {
+    // alert(item.name + ':' + item.num)
+    const { navigate } = this.props.navigation;
+    navigate('Cards', item)
+  }
 
   render() {
     const { navigate } = this.props.navigation;
@@ -12,6 +32,23 @@ class Desks extends Component {
           title="Add Desk"
           onPress={() =>
             navigate('AddDesk')
+          }
+        />
+
+        <FlatList
+          data={this.state.desks}
+          renderItem={({index, item}) =>
+            <TouchableOpacity key={index} activeOpacity={1} onPress={() => {
+              this.clickItem(item)
+            }}>
+              <View>
+                <View style={styles.item}>
+                  <Text>{item.name}</Text>
+                  <Text>Count:{item.num}</Text>
+                </View>
+                <Text style={styles.line}></Text>
+              </View>
+            </TouchableOpacity>
           }
         />
         <Button
@@ -26,3 +63,17 @@ class Desks extends Component {
 }
 
 export default Desks
+
+const styles = StyleSheet.create({
+  item: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 15,
+    backgroundColor: white,
+  },
+  line: {
+    height: 1,
+    backgroundColor: purple,
+  },
+})
