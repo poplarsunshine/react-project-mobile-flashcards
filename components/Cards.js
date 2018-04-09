1d53600f5dc817fd5d103ddb2cbcbb155ad45731
poplarsunshine <poplarsunshine@gmail.com>
2018-04-09 23:22:52 +0800

    Testing

--- components/Cards.js
+++ components/Cards.js
@@ -3,9 +3,23 @@ import { View, Text, Button } from 'react-native'
 import DeskItem from './DeskItem'
 
 class Cards extends Component {
+
+  state = {
+    cards: [
+      {question: 'Devin?', answer: 0},
+      {question: 'Jackson?', answer: 34},
+      {question: 'James?', answer: 3},
+      {question: 'Joel?', answer: 3},
+      {question: 'John?', answer: 45},
+      {question: 'Jillian?', answer: 0},
+      {question: 'Jimmy?', answer: 0},
+      {question: 'Julie?', answer: 25},
+    ]
+  }
+
   render() {
     const { navigate, state } = this.props.navigation;
-    const { name, num }  = state.params //取得正向传值
+    const { name, num }  = state.params
     // console.log('Cards state.params:', state.params);
 
     return (
@@ -25,7 +39,7 @@ class Cards extends Component {
         <Button
           title="Test"
           onPress={() =>
-            navigate('Test')
+            navigate('Test', this.state.cards)
           }
         />
       </View>
--- components/Test.js
+++ components/Test.js
@@ -2,10 +2,90 @@ import React, { Component } from 'react'
 import { View, Text, Button } from 'react-native'
 
 class Test extends Component {
+  state = {
+    testIndex: 0,
+    rightNum: 0,
+    wrongNum: 0,
+    showAnswer: false,
+  }
+
+  answerRight = () => {
+    const rightNum = this.state.rightNum + 1
+    this.setState(
+      {
+        rightNum: rightNum
+      }
+    )
+    this.setIndex()
+  }
+
+  answerWrong = () => {
+    const wrongNum = this.state.wrongNum + 1
+    this.setState(
+      {
+        wrongNum: wrongNum
+      }
+    )
+    this.setIndex()
+  }
+
+  setIndex = () => {
+    const index = this.state.testIndex + 1
+    this.setState(
+      {
+        testIndex: index
+      }
+    )
+  }
+
   render() {
+    const { testIndex } = this.state;
+    const { navigate, state } = this.props.navigation;
+    const cards  = state.params
+    const sum = cards.length
+
+    console.log('testIndex:', testIndex);
+    const card = cards[testIndex]
+    console.log('Test card:', card);
+
     return (
+      (testIndex + 1 > sum) ?
       <View>
-        <Text>Testing</Text>
+        <Text>Done</Text>
+        <Text>RightNum:{this.state.rightNum}</Text>
+        <Text>WrongNum{this.state.wrongNum}</Text>
+      </View>
+      :
+      <View>
+        <Text>Progress:{testIndex + 1}/{sum}</Text>
+        <Text>QUESTION:{card.question}</Text>
+
+        (this.state.showAnswer) ?
+        <View>
+          <Text>ANSWER:{card.answer}</Text>
+        </View>
+        :
+        <View>
+        <Button
+          title="Answer"
+          onPress={() => {
+
+          }}
+        />
+        </View>
+
+        <Button
+          title="Right"
+          onPress={() => {
+            this.answerRight()
+          }}
+        />
+        <Button
+          title="Wrong"
+          onPress={() => {
+            this.answerWrong()
+          }}
+        />
       </View>
     )
   }
